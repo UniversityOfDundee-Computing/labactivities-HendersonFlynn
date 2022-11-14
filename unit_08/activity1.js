2296085bd07d5708688d1658ed7c2e5392d8e19e
HendersonFlynn <2502464@dundee.ac.uk>
2022-11-14 14:51:55 +0000

    unit_08

---
+++ unit_08/activity1.js
@@ -0,0 +1,34 @@
+var inputBox = document.getElementById("Tip");
+var outPutBox = document.getElementById("processedTip");
+
+$("5Percent").click(FivePercentTip);
+
+document.getElementById("10Percent").addEventListener("click", TenPercentTip);
+document.getElementById("25Percent").addEventListener("click", TwentyFivePercentTip);
+
+document.getElementById("25PercentORTwo").addEventListener("click", TwentyFivePercentOrTwo);
+
+
+function FivePercentTip(){
+    outPutBox.textContent = "£" + Math.round(parseFloat(inputBox.value)*0.05*100)/100;
+}
+
+function TenPercentTip(){
+    outPutBox.textContent = "£" + Math.round(parseFloat(inputBox.value)*0.1*100)/100;
+}
+
+function TwentyFivePercentTip(){
+    outPutBox.textContent = "£" + Math.round(parseFloat(inputBox.value)*0.25*100)/100;
+}
+
+function TwentyFivePercentOrTwo(){
+    let TwentyFivePercentTip = Math.round(parseFloat(inputBox.value)*0.25*100)/100;
+    let TwoPlusTenPercent = 2 + Math.round(parseFloat(inputBox.value)*0.1*100)/100;
+
+    if (TwentyFivePercentTip > TwoPlusTenPercent){
+        outPutBox.textContent = "£" + TwentyFivePercentTip;
+        return;
+    }
+
+    outPutBox.textContent = "£" + TwoPlusTenPercent;
+}
