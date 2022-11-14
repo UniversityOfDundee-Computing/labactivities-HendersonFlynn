bc7fc259222efee60de27731326564dfe70f01e7
HendersonFlynn <2502464@dundee.ac.uk>
2022-11-14 16:42:26 +0000

    Hate JQuery

--- unit_08/activity1.js
+++ unit_08/activity1.js
@@ -1,34 +1,74 @@
-var inputBox = document.getElementById("Tip");
-var outPutBox = document.getElementById("processedTip");
+var inputBox = $("#Tip")[0];
+var outPutBox = $("#processedTip")[0];
 
-$("5Percent").click(FivePercentTip);
-
-document.getElementById("10Percent").addEventListener("click", TenPercentTip);
-document.getElementById("25Percent").addEventListener("click", TwentyFivePercentTip);
-
-document.getElementById("25PercentORTwo").addEventListener("click", TwentyFivePercentOrTwo);
-
+$("#5Percent").click(FivePercentTip);
+$("#10Percent").click(TenPercentTip);
+$("#25Percent").click(TwentyFivePercentTip);
+$("#25PercentORTwo").click(TwentyFivePercentOrTwo);
 
 function FivePercentTip(){
-    outPutBox.textContent = "£" + Math.round(parseFloat(inputBox.value)*0.05*100)/100;
+    let FivePercentValue = Math.round(parseFloat(inputBox.value)*0.05*100)/100;
+    //hate jQuery
+    if (FivePercentValue <= 5){
+        $("#processedTip").addClass("LessThanFive", 1000, returnToNormal )  
+    } else if (FivePercentValue >= 5 && FivePercentValue <= 10){
+        $("#processedTip").addClass("BetweenFiveAndTen", 1000, returnToNormal ) 
+    } else if (FivePercentValue >= 10){
+        $("#processedTip").addClass("GreaterThanTen", 1000, returnToNormal ) }
+
+    outPutBox.textContent = "£" + FivePercentValue;
 }
 
 function TenPercentTip(){
-    outPutBox.textContent = "£" + Math.round(parseFloat(inputBox.value)*0.1*100)/100;
+    let TenPercentValue = Math.round(parseFloat(inputBox.value)*0.1*100)/100;
+
+    if (TenPercentValue <= 5){
+        $("#processedTip").addClass("LessThanFive", 1000, returnToNormal )  
+    } else if (TenPercentValue >= 5 && TenPercentValue <= 10){
+        $("#processedTip").addClass("BetweenFiveAndTen", 1000, returnToNormal ) 
+    } else if (TenPercentValue >= 10){
+        $("#processedTip").addClass("GreaterThanTen", 1000, returnToNormal ) }
+
+
+
+
+    outPutBox.textContent = "£" + TenPercentValue;
+
+
+
 }
 
 function TwentyFivePercentTip(){
-    outPutBox.textContent = "£" + Math.round(parseFloat(inputBox.value)*0.25*100)/100;
+    let TwentyFivePercentValue = Math.round(parseFloat(inputBox.value)*0.25*100)/100;
+
+    if (TwentyFivePercentValue <= 5){
+        $("#processedTip").addClass("LessThanFive", 1000, returnToNormal )  
+    } else if (TwentyFivePercentValue >= 5 && TwentyFivePercentValue <= 10){
+        $("#processedTip").addClass("BetweenFiveAndTen", 1000, returnToNormal ) 
+    } else if (TwentyFivePercentValue >= 10){
+        $("#processedTip").addClass("GreaterThanTen", 1000, returnToNormal ) }
+
+    outPutBox.textContent = "£" + TwentyFivePercentValue;
 }
 
 function TwentyFivePercentOrTwo(){
     let TwentyFivePercentTip = Math.round(parseFloat(inputBox.value)*0.25*100)/100;
     let TwoPlusTenPercent = 2 + Math.round(parseFloat(inputBox.value)*0.1*100)/100;
 
+
+
     if (TwentyFivePercentTip > TwoPlusTenPercent){
         outPutBox.textContent = "£" + TwentyFivePercentTip;
         return;
+    } else {
+        outPutBox.textContent = "£" + TwoPlusTenPercent;
     }
 
-    outPutBox.textContent = "£" + TwoPlusTenPercent;
+
+}
+
+function returnToNormal(){
+    setTimeout(function() {
+        $("#processedTip").removeClass();
+    }, 1500);
 }
